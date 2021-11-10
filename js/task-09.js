e709396c703b778c320860c64031a04b0de2c70b
Pavel <Pavel.Debchuk@gmail.com>
2021-11-11 02:09:08 +0300

    dz-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+
+
+const colorjs = document.querySelector('.change-color');
+const codeColor = document.querySelectorAll('.color');
+const color = document.body;
+// codeColor = color.textContent;
+
+colorjs.addEventListener('click', getRandomHexColor);
+
+let colors;
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  color.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return color;
 }
+
+
+
+
